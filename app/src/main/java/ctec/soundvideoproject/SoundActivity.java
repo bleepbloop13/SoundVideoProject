d9f1dd22a48b0ff8fddb64eae630af6bb9922fb5
bleepbloop13 <rbak9218@740s-pl-161.local>
2015-11-12 12:33:44 -0700

    Basics of app added

--- app/src/main/java/ctec/soundvideoproject/SoundActivity.java
+++ app/src/main/java/ctec/soundvideoproject/SoundActivity.java
@@ -1,42 +1,136 @@
 package ctec.soundvideoproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.*;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class SoundActivity extends AppCompatActivity
+/**
+ * @author Rory Baker
+ * @version 1.0 Base fundamentals covered in video.
+ */
+
+public class SoundActivity extends Activity implements Runnable
 {
+    private Button startButton;
+    private Button stopButton;
+    private Button pauseButton;
+    private Button videoButton;
+    private MediaPlayer soundPlayer;
+    private SeekBar soundSeekBar;
+    private Thread soundThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound);
+
+        startButton = (Button) findViewById(R.id.playButton);
+        pauseButton = (Button) findViewById(R.id.pauseButton);
+        stopButton = (Button) findViewById(R.id.stopButton);
+        videoButton = (Button) findViewById(R.id.videoButton);
+        soundSeekBar = (SeekBar) findViewById(R.id.soundSeekBar);
+        soundPlayer = MediaPlayer.create(this.getBaseContext(), R.raw.in_the_garage);
+
+        setupListeners();
+
+        soundThread = new Thread(this);
+        soundThread.start();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
+    private void setupListeners()
     {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sound, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
+        startButton.setOnClickListener(new View.OnClickListener()
         {
-            return true;
-        }
+            @Override
+            public void onClick(View view)
+            {
+                soundPlayer.start();
+            }
+        });
 
-        return super.onOptionsItemSelected(item);
+        pauseButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                soundPlayer.pause();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                soundPlayer.stop();
+                soundPlayer = MediaPlayer.create(getBaseContext(), R.raw.in_the_garage);
+            }
+        });
+
+        videoButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent myIntent = new Intent(videoButton.getContext(), VideoActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+        soundSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                if (fromUser)
+                {
+                    soundPlayer.seekTo(progress);
+                }
+            }
+        });
+    }
+
+    /**
+     * Required since we are implementing Runnable.
+     * Allows the SeekBar to update.
+     */
+    @Override
+    public void run()
+    {
+        int currentPosition = 0;
+        int soundTotal = soundPlayer.getDuration();
+        soundSeekBar.setMax(soundTotal);
+
+        while (soundPlayer != null && currentPosition < soundTotal)
+        {
+            try
+            {
+                Thread.sleep(300);
+                currentPosition = soundPlayer.getCurrentPosition();
+            }
+            catch(InterruptedException soundException)
+            {
+                return;
+            }
+            catch(Exception otherException)
+            {
+                return;
+            }
+            soundSeekBar.setProgress(currentPosition);
+        }
     }
 }
--- app/src/main/java/ctec/soundvideoproject/VideoActivity.java
+++ app/src/main/java/ctec/soundvideoproject/VideoActivity.java
@@ -1,42 +1,57 @@
 package ctec.soundvideoproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.*;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class VideoActivity extends AppCompatActivity
+/**
+ * @author Rory Baker
+ * @version 1.0 Basics from video added
+ */
+
+public class VideoActivity extends Activity
 {
+    private VideoView myPlayer;
+    private Button returnButton;
+    private MediaController myVideoController;
+    private Uri videoLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        myPlayer = (VideoView) findViewById(R.id.videoViewer);
+        returnButton = (Button) findViewById(R.id.homeButton);
+
+        videoLocation = Uri.parse(("android resource://") + getPackageName() + "/" + R.raw.wombo_combo);
+        myVideoController = new MediaController(this);
+        //Prepare the video
+        setupMedia();
+        setupListeners();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
+    private void setupMedia()
     {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_video, menu);
-        return true;
+        myPlayer.setMediaController(myVideoController);
+        myPlayer.setVideoURI(videoLocation);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
+    private void setupListeners()
     {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
+        returnButton.setOnClickListener(new View.OnClickListener()
         {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+            @Override
+            public void onClick (View view)
+            {
+                Intent returnIntent = new Intent();
+                setResult(RESULT_OK, returnIntent);
+                finish();
+            }
+        });
     }
 }
